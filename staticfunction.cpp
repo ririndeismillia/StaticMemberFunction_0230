523ed686710534a1ac9d872f4f62433ce34eb68a
ririndeismillia <ririndeismillia@gmail.com>
2026-05-04 19:17:31 +0700

    memberi nilai pada setNim() untuk merubah nilai NIM

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -45,7 +45,10 @@ int main(){
     //pembuatan object dan pemberian nilai
     Mahasiswa mhs1("Sri Dadi");
     Mahasiswa mhs2("Budi Jatmiko");
-    
+    //memberi nilai pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNIm(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
 
     
 };
