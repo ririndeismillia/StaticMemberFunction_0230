50c2e489bbfda912c9c1cfa7279610989927cbc6
ririndeismillia <ririndeismillia@gmail.com>
2026-05-04 19:16:25 +0700

    pendefinisial prosedur - prosedur diluar class

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -27,3 +27,17 @@ class Mahasiswa{
         };
 };
 
+int Mahasiswa::nim = 0;
+
+//pendefinisial prosedur - prosedur diluar class
+void Mahasiswa::setID(){
+    id = ++nim;
+}
+
+void Mahasiswa::printAll(){
+
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+}
+
