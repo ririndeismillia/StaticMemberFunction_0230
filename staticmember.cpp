b236ad13f031842c129d8fdf90aa871e2b37e305
ririndeismillia <ririndeismillia@gmail.com>
2026-05-04 18:56:55 +0700

    membuat object dan memberi nilai

--- staticmember.cpp
+++ staticmember.cpp
@@ -27,3 +27,19 @@ void Mahasiswa::setID() {
     id = ++nim;
 };
 
+void Mahasiswa::printAll(){
+
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
+
+int main() {
+
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurnia");
+    Mahasiswa mhs4("Joko Purbo");
+
+
+};
